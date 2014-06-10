d5fb634ac54a50e2c3551be70b478fc258c6d445
Nikita Nikitin <berozzy@gmail.com>
2014-06-10 17:02:02 +0400

    added comments

--- gridddr.js
+++ gridddr.js
@@ -1,9 +1,22 @@
 ;
 (function($) {
+
+  /**
+   *  Create function alias for using like: $.gridddr()
+   *  which will automatically create needed nodes
+   *  @param {Object} options
+   *  @return {Object}
+   **/
   $.gridddr = function(options) {
     return $('body').gridddr(options, true);
   };
 
+  /**
+   *  Gridddr jQuery definition
+   *  @param {Object} options
+   *  @param {Boolean} global
+   *  @return {Object}
+   **/
   $.fn.gridddr = function(options, global) {
     var defaultSettings = {
       debug: false,
@@ -22,24 +35,39 @@
     };
 
     var
-      settings = $.extend(defaultSettings, options),
-      global = global || false,
-      private = {
-        wrapChildren: function($this) {
-          var children = $this.find(settings.itemClass);
-          if (children.size() > 0) {
-
+      settings = $.extend(defaultSettings, options), // Merging default settings with user settings (if defined)
+      global = global || false, // Use automatic node creation?
+      private = { // Private Gridddr functions
+        /**
+         *  Convert children objects into Gridddr items
+         *  @param {Object} $this
+         *  @return {Boolean}
+         **/
+        wrapItems: function($this) {
+          var items = $this.find(settings.itemClass);
+          if (items.size() > 0) {
+            items.each(function() {
+              if (!$(this).hasClass(settings.itemClass.slice(1))) {
+                $(this).addClass(settings.itemClass.slice(1));
+              };
+            });
           };
           return true;
         },
+        /**
+         *  Initialize Gridddr
+         *  @param {Integer} index
+         *  @param {Object} el
+         *  @return {Boolean}
+         **/
         inititalize: function(index, el) {
           var $this = $(el);
 
-          if (!$this.hasClass(settings.container)) {
-            $this.addClass(settings.container);
+          if (!$this.hasClass(settings.container.slice(1))) {
+            $this.addClass(settings.container.slice(1));
           };
 
-          private.wrapChildren($this);
+          private.wrapItems($this);
           if (!$this.data('inititalized')) {
             $this.data('inititalized', true);
           };
@@ -51,13 +79,17 @@
         }
       },
 
-      public = {
+      public = { // Public Gridddr methods. To user like var grid = $.gridddr(); grid.update();
+        /**
+         *  Returns used settings for the actual Gridddr instance
+         *  @return {Object}
+         **/
         getSettings: function() {
           return settings;
         }
       };
 
-    $.extend(this, public);
+    $.extend(this, public); // Merge settings
     $.each(this, private.inititalize);
     return this;
   };
